e8a6a07a124f526a543aa1e0d000adf0acd61dee
Michael Calabro <michael_calabro@college.harvard.edu>
2020-08-15 14:45:57 -0400

    more data

--- economic_viz_app/app.R
+++ economic_viz_app/app.R
@@ -21,29 +21,45 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
+        
+         selectInput("stat",
+                     "Select Economic Data to View",
+                     c("Fed Funds Rate" = "FEDFUNDS",
+                       "Unemployment Rate"  = "UNRATE",
+                       "ND000334Q",
+                       "CPIAUCSL",
+                       "M1V",
+                       "DGS2",
+                       "TEDRATE",
+                       "INTDSRUSM193N"),
+                     selected = "UNRATE"),
+         
          sliderInput("year",
                      "Select Years to View:",
-                     min = 1980,
+                     min = 1900,
                      max = 2020,
                      value = c(1980, 2020),
                      sep = "")
       ),
       
-      # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("distPlot"),
+         textOutput("testText")
       )
    )
 )
 
-getSymbols("FEDFUNDS", src = "FRED")
 
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
    
    output$distPlot <- renderPlot({
-      autoplot(FEDFUNDS[sprintf("%i/%i", input$year[1], input$year[2])])
+     my_data <- getSymbols(input$stat, src = "FRED", auto.assign = FALSE)
+     autoplot(my_data[sprintf("%i/%i", input$year[1], input$year[2])])
+   })
+   
+   output$testText <- renderText({
+     print(input$stat)
    })
 }
 
